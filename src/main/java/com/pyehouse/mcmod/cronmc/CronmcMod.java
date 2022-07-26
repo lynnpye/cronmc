e4d90edcb7674b6ba3ce3f751e33e749a60145cd
Lynn Pye <github@pyehouse.com>
2022-07-26 11:00:43 -0500

    made server only

--- src/main/java/com/pyehouse/mcmod/cronmc/CronmcMod.java
+++ src/main/java/com/pyehouse/mcmod/cronmc/CronmcMod.java
@@ -6,10 +6,12 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,6 +21,9 @@ public class CronmcMod {
     public static final String MODID = "cronmc";
 
     public CronmcMod() {
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 
         registerConfigs();
 
